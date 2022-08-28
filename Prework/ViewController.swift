bc2ef6b971c3b1a2149138650e49c565ada54797
Siyuan PENG <16830124+sdotpeng@users.noreply.github.com>
2022-08-28 21:59:24 +0700

    Update

---
+++ Prework/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Siyuan Peng on 8/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var numPeopleLabel: UILabel!
+    @IBOutlet weak var numPeopleStepper: UIStepper!
+    @IBOutlet weak var eachPersonAmountLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let numPeople = Int(numPeopleStepper.value)
+        
+        // Get total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        let each = total / Double(numPeople)
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+     
+        eachPersonAmountLabel.text = String(format: "$%.2f", each)
+        
+        
+    }
+    
+    @IBAction func updateNum(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let numPeople = Int(numPeopleStepper.value)
+        
+        // Get total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        let each = total / Double(numPeople)
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+     
+        eachPersonAmountLabel.text = String(format: "$%.2f", each)
+        numPeopleLabel.text = String(numPeople)
+        
+    }
+}
+
